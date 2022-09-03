d2ff405eea954e1b37fc6dc42912a247dbb49822
astridpettersen <s354534@oslomet.no>
2022-09-03 11:43:17 +0200

    Make a Client class. Make a ClientController. Generate a table in index.html with the objects in the ClientController using js.

---
+++ Model/Client.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Model
+{
+    public class Client
+    {
+        public string name { get; set; }
+        public string address { get; set; }
+    }
+}
+
---
+++ wwwroot/js/index.js
@@ -0,0 +1,25 @@
+﻿$(function () {
+    getAllClients();
+});
+
+function getAllClients() {
+    $.get("Client/getAllClients", function (clients) {
+        formatClients(clients);
+    });
+}
+
+function formatClients(clients) {
+    let out = "<table class='table table-striped'>" +
+        "<tr>" +
+        "<th>Name</th><th>Address</th><th></th><th></th>" +
+        "</tr>";
+    for (let client of clients) {
+        out += "<tr>" +
+            "<td>" + client.name + "</td>" +
+            "<td>" + client.address + "</td>" +
+            "</tr>";
+    }
+    out += "</table>";
+    $("#clients").html(out);
+    
+}
